7bee70e59041600c29f2ca89fa5caab08a4489c4
jezyq14 <michal.muzyka@proton.me>
2025-05-17 13:44:15 +0200

    Struktura

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wypożyczalnia samochodów</title>
+    <link rel="stylesheet" href="styls.css">
+</head>
+
+<body>
+    <!-- Nagłówek -->
+    <header>
+        <img src="logo.png" alt="nasze logo">
+    </header>
+
+    <!-- Menu -->
+    <nav>
+        <h1>Wypożyczalnia samochodów</h1>
+        <table>
+            <tr>
+                <td class="nav-table-td"><a href="index.php">Strona główna</a></td>
+                <td class="nav-table-td"><a href="dodaj.php">Dodaj wypożyczenie</a></td>
+            </tr>
+        </table>
+    </nav>
+
+    <main>
+        <!-- Lewa sekcja -->
+        <section class="left">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nazwisko</th>
+                        <th>Imię</th>
+                        <th>Marka</th>
+                        <th>Model</th>
+                        <th>Numer Rej.</th>
+                        <th>Data Wyp.</th>
+                        <th>Data Zwr.</th>
+                        <th>Koszt</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Skrypt 2 -->
+                    <?php
+                    ?>
+                </tbody>
+            </table>
+        </section>
+
+        <!-- Prawa sekcja -->
+        <aside class="right">
+            <section class="right-1">
+                <img src="auto.png" alt="maluch">
+            </section>
+            <section class="right-2">
+                <h3>Zawsze niskie ceny!</h3>
+                <h4>Ilość samochodów w bazie</h4>
+
+                <!-- Skrypt 1 -->
+                <?php
+
+
+
+                ?>
+            </section>
+        </aside>
+    </main>
+
+    <!-- Stopki -->
+    <footer>
+        <section class="footer-1">
+            <p>Warszawa <br> ul. Wiejska 12 <br> tel. 123456789</p>
+        </section>
+        <section class="footer-2">
+            <p>Autor strony: <br> 00000000000</p>
+        </section>
+    </footer>
+</body>
+
+</html>
