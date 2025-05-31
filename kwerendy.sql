68a5b9a686dd734cc7128d213cd5198d9c621270
jezyq14 <michal.muzyka@proton.me>
2025-05-31 15:04:00 +0200

    update kwerend, bo coś nie działało pod windowsem (kocham linuxa, naprawdę)

--- kwerendy.sql
+++ kwerendy.sql
@@ -9,14 +9,14 @@ SELECT k.IMIE, k.NAZWISKO FROM klienci k NATURAL JOIN wypozyczenia w WHERE w.DAT
 SELECT * FROM samochody s WHERE s.KRAJ_PROD = "Niemcy" AND s.KOSZT_DNIA > 20;
 
 /* 4. Dodaj użytkownika „wypozyczajacy” z hasłem zaq1@WSX. */
-CREATE USER 'wypozyczajacy'@'%' IDENTIFIED BY 'zaq1@WSX';
+CREATE USER 'wypozyczajacy'@'127.0.0.1' IDENTIFIED BY 'zaq1@WSX';
 
 /* 5. Wyświetl imię, nazwisko, marka, model, nr_rej, data_wyp, data_zw i koszt dla wszystkich
 samochodów, klientów i wypożyczeń (relacja między trzema tabelami). */
 SELECT k.IMIE, k.NAZWISKO, s.MARKA, s.MODEL, s.NR_REJ, w.DATA_WYP, w.DATA_ZWR, w.KOSZT FROM samochody s NATURAL JOIN wypozyczenia w NATURAL JOIN klienci k;
 
 /* 6. Dodaj funkcję automatycznej numeracji dla kolumny id_wyp w tabeli wypożyczenia. */
-ALTER TABLE `wypozyczenia` CHANGE `ID_WYP` `ID_WYP` INT NOT NULL AUTO_INCREMENT; 
+ALTER TABLE `wypozyczenia` CHANGE `ID_WYP` `ID_WYP` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`ID_WYP`); 
 
 /* 7. Wstaw do tabeli wypożyczenie – id_kli = 100, id_sam = 15, data_wyp = dziś, data_zwr = za
 miesiąc i koszt 1000 */
